548fb962e6eaf4e0b8700da7f3b2fecf23e6086f
nisar198 <nisar198@hotmail.com>
2023-11-06 12:48:11 +0000

    test123

--- modules/snowflake_resources/_variables.tf
+++ modules/snowflake_resources/_variables.tf
@@ -10,3 +10,9 @@ variable "time_travel_in_days" {
   type        = number
   description = "Number of days for time travel feature"
 }
+
+variable "snowflake_private_key" {
+  type        = string
+  description = "Private key used to access Snowflake"
+  sensitive   = true
+}
