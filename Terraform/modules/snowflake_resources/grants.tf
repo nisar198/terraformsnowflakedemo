7b1fcc9f7ee9f910e09a57fe88455bd4535db115
nisar198 <nisar198@hotmail.com>
2023-11-06 21:22:53 +0000

    TEST123

--- Terraform/modules/snowflake_resources/grants.tf
+++ Terraform/modules/snowflake_resources/grants.tf
@@ -3,17 +3,24 @@ resource "snowflake_database_role" "db_role" {
   name     = "TF_DEMO_READER"
   comment  = "my db role"
 }
-#resource "snowflake_role_grants" "demo_role_grants"{
- #   role_name= snowflake_database_role.db_role.name
-  #  users = "NISAR"
 
-#}
+resource "snowflake_database_role" "db_role_wr" {
+  database = snowflake_database.tf_demo.name
+  name     = "TF_DEMO_WR"
+  comment  = "my db role WR"
+}
+
+resource "snowflake_role_grants" "demo_role_grants"{
+    role_name= ["TF_DEMO_READER", "TF_DEMO_WR"]
+    users = "NISAR"
+
+}
 
 resource "snowflake_database_grant" "database_ro_grant" {
   database_name = snowflake_database.tf_demo.name
 
   privilege = "USAGE"
-  roles     = ["TF_DEMO_READER"]
+  roles     = ["TF_DEMO_READER", "TF_DEMO_WR"]
 }
 
 resource "snowflake_schema_grant" "schema_ro_grant" {
@@ -21,7 +28,7 @@ resource "snowflake_schema_grant" "schema_ro_grant" {
   schema_name   = snowflake_schema.tf_schema.name
 
   privilege = "USAGE"
-  roles     = ["TF_DEMO_READER"]
+  roles     = ["TF_DEMO_READER", "TF_DEMO_WR"]
 }
 
 resource "snowflake_table_grant" "table_ro_grant" {
@@ -30,6 +37,18 @@ resource "snowflake_table_grant" "table_ro_grant" {
 
   privilege = "SELECT"
   roles     = ["TF_DEMO_READER"]
+
+  on_future         = true
+  with_grant_option = false
+  #on_all            = false
+}
+
+resource "snowflake_table_grant" "table_wr_grant" {
+  database_name = snowflake_database.tf_demo.name
+  schema_name   = snowflake_schema.tf_schema.name
+
+  privilege = ["SELECT", "INSER", "CREATE TABLE"]
+  roles     = ["TF_DEMO_WR"]
 
   on_future         = true
   with_grant_option = false
@@ -51,5 +70,5 @@ resource "snowflake_view_grant" "view_ro_grant" {
 resource "snowflake_warehouse_grant" "warehouse_grant" {
   warehouse_name = snowflake_warehouse.task_warehouse.name
   privilege      = "USAGE"
-  roles          = ["TF_DEMO_READER"]
+  roles          = ["TF_DEMO_READER", "TF_DEMO_WR"]
 }
