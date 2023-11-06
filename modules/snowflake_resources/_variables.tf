644b5bfc51acf97ae470f4329f4218f8cdbe30e7
nisar198 <nisar198@hotmail.com>
2023-11-06 15:50:10 +0000

    test123

--- modules/snowflake_resources/_variables.tf
+++ modules/snowflake_resources/_variables.tf
@@ -10,3 +10,4 @@ variable "time_travel_in_days" {
   type        = number
   description = "Number of days for time travel feature"
 }
+
--- modules/snowflake_resources/db_schema.tf
+++ modules/snowflake_resources/db_schema.tf
@@ -3,8 +3,8 @@ resource "snowflake_database" "tf_demo" {
   data_retention_time_in_days = var.time_travel_in_days
 }
 
-resource "snowflake_schema" "tf_schema" {
-  database = snowflake_database.tf_demo.name
-  name     = "SCHEMA1"
+resource "snowflake_schema" "tf_demo_schema" {
+  database = snowflake_database.tf_demo_database.name
+  name     = "TF_DEMO"
 }
 
