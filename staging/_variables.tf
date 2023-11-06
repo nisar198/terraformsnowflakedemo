8dad0cf1940b2374de75f405852a9d22207da752
nisar198 <nisar198@hotmail.com>
2023-11-06 12:42:00 +0000

    snowflake_private_key is added

--- staging/_variables.tf
+++ staging/_variables.tf
@@ -13,3 +13,9 @@ variable "time_travel_in_days" {
   description = "Number of days for time travel feature"
   default     = 1
 }
+
+variable "snowflake_private_key" {
+  type        = string
+  description = "Private key used to access Snowflake"
+  sensitive   = true
+}
